b0a0eaf48ad52c48530593127d42007fb381e345
kheanggg <mengkheang1202@gmail.com>
2025-07-08 15:04:30 +0700

    Create Homepage & PhotoCard

---
+++ app/home/page.js
@@ -0,0 +1,13 @@
+import PhotoCard from "@component/PhotoCard"
+
+export default function Home() {
+    return(
+        <>
+            <div className="flex gap-5">
+                <PhotoCard title="Car 1" image="/car_1.jpg"/>
+                <PhotoCard title="Car 2" image="/car_2.jpg"/>
+            </div>
+            <a type="button" href="/">Click here to /</a>
+        </>
+    )
+}
